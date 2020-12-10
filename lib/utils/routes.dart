2d9a7def017bff5fa0b8dab28af4392975dbba6f
Amadou91 <46040590+Amadou91@users.noreply.github.com>
2020-12-10 10:47:48 -0500

    3

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sim/pages/inductions.dart';
+import 'package:sim/pages/questions.dart';
+import 'package:sim/pages/quiz.dart';
+import 'package:sim/pages/quizzes.dart';
+import 'package:sim/pages/sites.dart';
+import 'package:sim/pages/uploadphoto.dart';
+import 'package:sim/pages/users.dart';
 import '../pages/home.dart';
 import '../pages/login.dart';
 
@@ -6,4 +13,12 @@ final routes = {
   '/login':         (BuildContext context) => new LoginScreen(),
   '/home':         (BuildContext context) => new HomeScreen(),
   '/' :          (BuildContext context) => new LoginScreen(),
+  '/users': (BuildContext context) => new PageUsers(),
+  '/inductions': (BuildContext context) => new PageInductions(),
+  '/questions': (BuildContext context) => new PageQuestions(),
+ //'/quiz': (BuildContext context) => new PageQuiz(),
+  '/quiz': (BuildContext context) => new QuizApp(),
+  '/quizzes': (BuildContext context) => new PageQuizzes(),
+  '/sites': (BuildContext context) => new PageSites(),
+  '/uploadphoto': (BuildContext context) => new PageUploadPhoto(),
 };
